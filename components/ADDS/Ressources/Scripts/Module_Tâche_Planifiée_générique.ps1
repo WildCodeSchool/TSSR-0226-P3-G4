acf061b085694c459c20bceae923a87a601321f7
JeMa9701 <Jeremy-machado@outlook.fr>
2026-07-11 20:26:05 +0200

    Update Module_Tâche_Planifiée_générique.ps1

--- components/ADDS/Ressources/Scripts/Module_Tâche_Planifiée_générique.ps1
+++ components/ADDS/Ressources/Scripts/Module_Tâche_Planifiée_générique.ps1
@@ -8,22 +8,22 @@ $ScriptName = "TachePlanifieeGenerique"
 Write-XTechLog -ScriptName $ScriptName -Level "INFO" -Message "=== Démarrage du script $ScriptName ==="
 
 try {
-    #Défini ce que je veux executer
+    # Défini ce que je veux executer
     $action = New-ScheduledTaskAction -Execute "powershell.exe" `
         -Argument "-NoProfile -ExecutionPolicy Bypass -File `"C:\chemin\DuScript.ps1`""
 
-    #Défini quand la tache doit s'executer
+    # Défini quand la tache doit s'executer
     $trigger = New-ScheduledTaskTrigger -Daily -At 11pm
 
-    #Définit quel compte execute la tâche et avec quels droits.
+    # Définit quel compte execute la tâche et avec quels droits.
     $principal = New-ScheduledTaskPrincipal -UserId "XTECH\svc-adtasks" `
         -LogonType Password -RunLevel Highest
 
-    #Multiple execution en cas de crash ou de serveur eteint au moment où la tâche devrait se lancer
+    # Multiple execution en cas de crash ou de serveur eteint au moment où la tâche devrait se lancer
     $settings = New-ScheduledTaskSettingsSet -StartWhenAvailable `
         -RestartCount 3 -RestartInterval (New-TimeSpan -Minutes 5)
 
-    #Enregistre la tâche dans le planificateur
+    # Enregistre la tâche dans le planificateur
     Register-ScheduledTask -TaskName "Nom de la tache a executer" `
         -Action $action -Trigger $trigger -Principal $principal -Settings $settings `
         -Description "description de la tache a executer" -ErrorAction Stop
